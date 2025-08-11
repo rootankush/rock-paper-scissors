f7ec1400722e3bf12b7188a1cfd3e2163aa2ff3e
rootankush <rootankushprompt@gmail.com>
2025-08-11 23:47:17 +0530

    Added ui to the website

--- main.js
+++ main.js
@@ -1,65 +1,76 @@
-let ComputerChoice;
-let PlayerChoice;
+let display = document.getElementById("display");
+let scoreDisplay = document.getElementById("scoreDisplay");
+
+let ComputerChoice = "";
+let PlayerChoice = "";
 let computerScore = 0;
 let playerScore = 0;
 
 function getComputerChoice() {
-  ComputerChoice = ['rock','paper','scissor'];
+  ComputerChoice = ["rock", "paper", "scissor"];
   let len = ComputerChoice.length;
   ComputerChoice = ComputerChoice[Math.floor(Math.random() * len)];
 }
 
 function getPlayerChoice() {
-  PlayerChoice = prompt("Rock or Paper or Scissor");
+  function choseRock() {
+    PlayerChoice = "rock";
+  }
+  choseRock();
+
+  function chosePaper() {
+    PlayerChoice = "paper";
+  }
+  chosePaper();
+
+  function choseScissor() {
+    PlayerChoice = "scissor";
+  }
+  choseScissor();
 }
 
 function gameLogic() {
-  let player = PlayerChoice.toLowerCase();
-  let computer = ComputerChoice.toLowerCase();
-    if(player == 'rock' && computer == 'paper'){
-      console.log("Computer Chose Paper and Player Chose Rock");
-      console.log("Computer Wins! Paper beats Rock");
-      computerScore++;
-    }
-    else if(player == 'paper' && computer == 'scissor'){
-      console.log("Computer Chose Scissor and Player Chose Paper");
-      console.log("Computer Wins! Scissor beats Paper");
-      computerScore++;
-    }
-    else if(player == 'scissor' && computer == 'rock'){
-      console.log("Computer Chose Rock and Player Chose Scissor");
-      console.log("Computer Wins! Rock beats Scissor");
-      computerScore++;
-    }
-    else if(player == 'paper' && computer == 'rock'){
-      console.log("Player Chose Paper and Computer Chose Rock");
-      console.log("Player Wins! Paper beats Rock");
-      playerScore++;
-    }
-    else if(player == 'scissor' && computer == 'paper'){
-      console.log("Player Chose Scissor and Computer Chose Paper");
-      console.log("Player Wins! Scissor beats Paper");
-      playerScore++;
-    }
-    else if(player == 'rock' && computer == 'scissor'){
-      console.log("Player Chose Rock and Computer Chose Scissor");
-      console.log("Player Wins! Rock beats Scissor");
-      playerScore++;
-    }
-    else {
-      console.log("Its a TIE");
-    }
+  let player = PlayerChoice;
+  let computer = ComputerChoice;
+  if (player == "rock" && computer == "paper") {
+    display.textContent = "Computer Wins! Paper beats Rock";
+    computerScore++;
+  } else if (player == "paper" && computer == "scissor") {
+    display.textContent = "Computer Wins! Scissor beats Paper";
+    computerScore++;
+  } else if (player == "scissor" && computer == "rock") {
+    display.textContent = "Computer Wins! Rock beats Scissor";
+    computerScore++;
+  } else if (player == "paper" && computer == "rock") {
+    display.textContent = "Player Wins! Paper beats Rock";
+    playerScore++;
+  } else if (player == "scissor" && computer == "paper") {
+    display.textContent = "Player Wins! Scissor beats Paper";
+    playerScore++;
+  } else if (player == "rock" && computer == "scissor") {
+    display.textContent = "Player Wins! Rock beats Scissor";
+    playerScore++;
+  } else {
+    display.textContent = "It's a TIE";
+  }
 }
+
+let rockBtn = document
+  .getElementById("rockBtn")
+  .addEventListener("click", getPlayerChoice);
+let paperBtn = document
+  .getElementById("paperBtn")
+  .addEventListener("click", getPlayerChoice);
+let scissorBtn = document
+  .getElementById("scissorBtn")
+  .addEventListener("click", getPlayerChoice);
 
 const computerSelection = getComputerChoice();
 const playerSelection = getPlayerChoice();
 
-for(let i=0;i<5;i++){
+for (let i = 0; i < 6; i++) {
   getComputerChoice();
   getPlayerChoice();
   gameLogic(playerSelection, computerSelection);
-  console.log("Computer: " + computerScore);
-  console.log("Player: " + playerScore);
+  scoreDisplay.textContent = `Computer: ${computerScore} || Player: ${playerScore}`;
 }
-
-
